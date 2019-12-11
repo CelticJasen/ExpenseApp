9e901730e8148032857acfb82e827850e9502296
FatedSoul <celticjasen@hotmail.com>
2019-12-11 15:13:15 -0600

    Add project files.

---
+++ ExpenseAppHome.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ExpenseApp
+{
+    /// <summary>
+    /// Interaction logic for ExpenseAppHome.xaml
+    /// </summary>
+    public partial class ExpenseAppHome : Page
+    {
+        public ExpenseAppHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            ExpenseReportPage expenseReportPage = new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+
+        }
+    }
+}
